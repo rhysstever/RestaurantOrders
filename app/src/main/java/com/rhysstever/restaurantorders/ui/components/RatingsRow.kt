30ab4fd80ddf116145296696b7dfa7b5e77c45dd
Stever, Rhys <Rhys_Stever@comcast.com>
2025-08-04 15:08:41 -0400

    Updates to RatingRow component
    Have it use the new AppIcons

--- app/src/main/java/com/rhysstever/restaurantorders/ui/components/RatingsRow.kt
+++ app/src/main/java/com/rhysstever/restaurantorders/ui/components/RatingsRow.kt
@@ -2,14 +2,9 @@ package com.rhysstever.restaurantorders.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredSizeIn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,11 +16,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.clearAndSetSemantics
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rhysstever.restaurantorders.R
+import com.rhysstever.restaurantorders.ui.theme.AppIcon
+import com.rhysstever.restaurantorders.ui.theme.AppIcons
 import com.rhysstever.restaurantorders.ui.theme.Typography
 
 @Composable
@@ -37,55 +35,48 @@ fun RatingsRow(
     enabled: Boolean = true
 ) {
     val clampedRating = rating?.coerceIn(1, 5) ?: 0
+    val contentDescription = "$ratingTitle ${pluralStringResource(R.plurals.stars, clampedRating, clampedRating)}"
 
-    Column(
+    Row(
         modifier = modifier
+            .semantics {
+                this.contentDescription = contentDescription
+            }
             .fillMaxWidth()
             .semantics(mergeDescendants = true) {}
             .clickable(
                 enabled = enabled,
                 role = Role.Button
             ) {
-                if(clampedRating == 5) {
+                if (clampedRating == 5) {
                     onRatingChanged(0) // Reset rating if already at max
                 } else {
                     onRatingChanged(clampedRating + 1)
                 }
-            }
+            },
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = ratingTitle,
+            text = "${ratingTitle}:",
+            modifier = Modifier.clearAndSetSemantics {},
             style = Typography.bodyLarge,
         )
-
-        val contentDescription = pluralStringResource(R.plurals.stars, clampedRating, clampedRating)
-
-        Row(
-            modifier = Modifier
-                .semantics {
-                    this.contentDescription = contentDescription
-                }
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            repeat(5) { index ->
-                Icon(
-                    imageVector = Icons.Filled.Star,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .requiredSizeIn(
-                            minWidth = 24.dp,
-                            minHeight = 24.dp
-                        )
-                        .padding(12.dp),
-                    tint = if (index < clampedRating) {
-                        Color(0xFFFFBB00)
-                    } else {
-                        Color.Black
-                    },
-                )
-            }
+        repeat(5) { index ->
+            AppIcon(
+                icon = if(index < clampedRating) {
+                    AppIcons.StarFilled
+                } else {
+                    AppIcons.StarOutline
+                },
+                contentDescription = null,
+                modifier = Modifier.padding(4.dp),
+                tint = if (index < clampedRating) {
+                    Color(0xFFFFBB00)
+                } else {
+                    Color.Black
+                },
+            )
         }
     }
 }
